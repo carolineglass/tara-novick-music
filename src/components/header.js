26fa88f924eab2630f35b1382c7ebff8eda1f73c
carolineglass <carolineglass89@gmail.com>
2020-10-28 18:48:01 -0400

    add header styles

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import headerStyles from './header.module.scss'
 
-const Footer = () => {
+const Header = () => {
     return (
         <header>
-            <h1><Link to="/" style={{textDecoration: 'none', color: '#5E747F', fontSize: '80px'}}>Tara Novick</Link></h1>
+            <h1><Link className={headerStyles.name} to="/">Tara Novick</Link></h1>
             <nav>
-                <ul>
-                    <li><Link to="/music">Music</Link></li>
-                    <li><Link to="/gallery">Gallery</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
+                <ul className={headerStyles.ul}>
+                    <li><Link className={headerStyles.link} to="/music">Music</Link></li>
+                    <li><Link className={headerStyles.link} to="/gallery">Gallery</Link></li>
+                    <li><Link className={headerStyles.link} to="/about">About</Link></li>
+                    <li><Link className={headerStyles.link} to="/contact">Contact</Link></li>
                 </ul>
             </nav>
         </header>
     )
 }
 
-export default Footer
+export default Header
