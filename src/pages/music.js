a8117acd22ba492cf082c036146c3d02fa923b0e
carolineglass <carolineglass89@gmail.com>
2021-01-18 12:12:00 -0500

    music styles

--- src/pages/music.js
+++ src/pages/music.js
@@ -5,9 +5,11 @@ import musicStyles from '../styles/music.module.scss'
 const MusicPage = () => {
     return (
         <Layout>
-        <div className={musicStyles.soundcloudContainer}>
-            <div className={musicStyles.soundcloudDiv}><iframe title="tara song" width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/277001159&color=%236c7484&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe></div>
-            <div className={musicStyles.soundcloudDiv}><iframe title="tara song" width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/276997734&color=%236c7484&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe></div>
+        <div className={musicStyles.musicDiv}>
+            <div className={musicStyles.soundcloudContainer}>
+                <div className={musicStyles.soundcloudDiv}><iframe title="tara song" width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/277001159&color=%236c7484&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe></div>
+                <div className={musicStyles.soundcloudDiv}><iframe title="tara song" width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/276997734&color=%236c7484&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe></div>
+            </div>
         </div>
         </Layout>
     )
